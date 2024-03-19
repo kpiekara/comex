023441ed5b87d16f553f2c04daa982f69892c68c
Kamil Piekara <kamil.piekara@madmax.consulting>
2024-03-19 08:36:00 +0100

    Add FPS counter

---
+++ game/Game/Prefabs/FpsCounter.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+using Game.Engine.Components;
+using Game.Engine.Tree;
+using SFML.Graphics;
+
+namespace Game.Prefabs;
+
+public class FpsCounter : GameObject
+{
+    private readonly Label _label;
+    
+    public FpsCounter(Font font)
+    {
+        _label = Add(new Label(font));
+        _label.SetPosition(0, 0);
+        _label.SetColor(Color.Red);
+        _label.SetBold(true);
+    }
+
+    public override void Update(float dt)
+    {
+        _label.SetText(Math.Floor(1 / dt).ToString(CultureInfo.InvariantCulture));
+    }
+}
--- game/Game/Program.cs
+++ game/Game/Program.cs
@@ -5,7 +5,9 @@ using Game.Resources;
 var gameRunner = new GameBuilder().BuildRunner();
 
 gameRunner.AssetManager.AddTileSet(@"..\..\..\..\..\resources\tileset1.bmp", TileSets.BaseTileSet.Name, TileSets.BaseTileSet.TileSize);
+gameRunner.AssetManager.AddFont(@"..\..\..\..\..\resources\ShareTechMono-Regular.ttf", Fonts.Basic);
 
-gameRunner.GameTree.Add(new Map(100, 100, gameRunner.AssetManager));
+gameRunner.GameTree.Add(new Map(200, 200, gameRunner.AssetManager));
+gameRunner.GameCanvas.Add(new FpsCounter(gameRunner.AssetManager.GetFont(Fonts.Basic)));
 
 gameRunner.Run();
