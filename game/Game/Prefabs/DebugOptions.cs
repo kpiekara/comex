839e89e96bc42803f30eb74b4457918364c1ca86
Kamil Piekara <kamil.piekara@madmax.consulting>
2024-03-24 01:45:10 +0100

    Add debug options

---
+++ game/Game/Prefabs/DebugOptions.cs
@@ -0,0 +1,25 @@
+﻿using Game.Engine.Components;
+using Game.Engine.Configs;
+using Game.Engine.Tree;
+using SFML.Window;
+
+namespace Game.Prefabs;
+
+public class DebugOptions : GameObject
+{
+    public DebugOptions(IGameSettings settings)
+    {
+        var keyboard = Add(new KeyboardInput());
+        keyboard.Released += code =>
+        {
+            switch (code)
+            {
+                case Keyboard.Key.F1:
+                    settings.LimitCameraToMap = !settings.LimitCameraToMap;
+                    break;
+                default:
+                    break;
+            }
+        };
+    }
+}
--- game/Game/Program.cs
+++ game/Game/Program.cs
@@ -8,6 +8,7 @@ gameRunner.AssetManager.AddTileSet(@"..\..\..\..\..\resources\tileset1.bmp", Til
 gameRunner.AssetManager.AddFont(@"..\..\..\..\..\resources\ShareTechMono-Regular.ttf", Fonts.Basic);
 
 gameRunner.GameTree.Add(new Map(300, 300, gameRunner.AssetManager));
+gameRunner.GameTree.Add(new DebugOptions(gameRunner.Settings));
 gameRunner.GameCanvas.Add(new FpsCounter(gameRunner.AssetManager.GetFont(Fonts.Basic)));
 
 gameRunner.Camera.SetCameraAreaSize(19200, 19200);
